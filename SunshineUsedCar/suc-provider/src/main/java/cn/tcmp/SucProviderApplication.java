0d1c9de1281f4f6a7bed2154ecf6f00fe0c30d83
AnRan-727 <17600000142@163.com>
2019-09-09 19:14:34 +0800

    用户Service

--- SunshineUsedCar/suc-provider/src/main/java/cn/tcmp/SucProviderApplication.java
+++ SunshineUsedCar/suc-provider/src/main/java/cn/tcmp/SucProviderApplication.java
@@ -1,9 +1,11 @@
 package cn.tcmp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.tcmp.dao")
 public class SucProviderApplication {
 
     public static void main(String[] args) {
---
+++ SunshineUsedCar/suc-provider/src/main/java/cn/tcmp/dao/CarUserMapper.java
@@ -0,0 +1,24 @@
+package cn.tcmp.dao;
+
+import cn.tcmp.entity.CarUser;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnRan
+ * Date: 2019/9/9
+ */
+public interface CarUserMapper {
+
+    //客户登录
+    CarUser carUserLogin(CarUser carUser);
+
+    //用户是否存在
+    CarUser existCarUser(String userPhone);
+
+    //客户注册
+    int carUserRegister(CarUser carUser);
+
+    //客户个人信息
+    CarUser detailCarUser(Integer id);
+
+}
